1c256a026af9acd433f7eca0eea1637b63d3f92d
bt-glv <brunotg02@gmail.com>
2025-05-14 21:07:11 -0300

    fix(Telescope): no quicklist send auto open
    - when sending results to the quickfix list, it wont
    immediately open it with ':copen'.
    - refactored some comments.

--- lua/plugin/_telescope.lua
+++ lua/plugin/_telescope.lua
@@ -9,26 +9,37 @@ return {
 		local actions = require('telescope.actions')
 		local action_state = require('telescope.actions.state')
 		require('telescope').setup{
+
 		  defaults = {
 			mappings = {
+
 			  i = {
 				["<C-y>"] = function(prompt_bufnr)
 				  local selection = action_state.get_selected_entry()
 				  actions.close(prompt_bufnr)
 				  vim.api.nvim_command('let @" = @' .. selection.value)
 				end,
+				["<M-q>"] = 'send_selected_to_qflist',
+				["<C-Q>"] = 'send_to_qflist',
 			  },
+
+			  n = {
+				["<M-q>"] = 'send_selected_to_qflist',
+				["<C-Q>"] = 'send_to_qflist',
+			  }
+
 			},
-		  }
+		  } -- End
+
 		}
 
-		km("n", "<leader>ff", 	":Telescope find_files<CR>") 		-- Fuzzy finds and open file 
-		km("n", "<leader>fb", 	":Telescope buffers<CR>") 	-- Fuzzy finds and open buffer
-		km("n", "<leader>fl", 	":Telescope live_grep<CR>") 		-- Fuzzy finds a line in your current text buffer
-		km("n", "<leader>fm", 	":Telescope marks<CR>") 		-- Fuzzy finds marks 
-		km("n", "<leader>fr", 	":Telescope registers<CR>") 		-- Finds files that contain a ripgrep match
-		-- km("n", "<leader>rr", 	":Telescope registers<CR>") 		-- Finds files that contain a ripgrep match
-		km("n", "<leader>fh", 	":Telescope current_buffer_fuzzy_find<CR>") 		-- Finds files that contain a ripgrep match
+		km("n", "<leader>ff", 	":Telescope find_files<CR>") 	-- finds a file and opens it
+		km("n", "<leader>fl", 	":Telescope live_grep<CR>") 	-- finds files that contain the search
+		km("n", "<leader>fm", 	":Telescope marks<CR>") 		-- finds <<MARKS>>
+		km("n", "<leader>fr", 	":Telescope registers<CR>") 	-- lets you select a register
+		km("n", "<leader>fb", 	":Telescope buffers<CR>") 		-- finds and opens a buffer
+
+		km("n", "<leader>fh", 	":Telescope current_buffer_fuzzy_find<CR>") 		-- finds files that contain a ripgrep match
 
 	end
 }
