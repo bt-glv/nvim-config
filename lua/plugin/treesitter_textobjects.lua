6d8610744d10e8fead4ee5adbff280bcb5521567
glv <glv@nk.desk>
2025-06-27 19:40:42 -0300

    feat(treesitter-textobjects): Add better keymaps & more motions

--- lua/plugin/treesitter_textobjects.lua
+++ lua/plugin/treesitter_textobjects.lua
@@ -5,65 +5,86 @@
 --
 
 return {
-	'nvim-treesitter/nvim-treesitter-textobjects',
-	lazy = false,
-	dependencies = {"nvim-treesitter/nvim-treesitter"},
-	priority = 100,
-	config = function()
-		require'nvim-treesitter.configs'.setup {
-		  textobjects = {
-			select = {
-			  enable = true,
-			  lookahead = true,
-			  keymaps = {
-				["af"] = "@function.outer",
-				["if"] = "@function.inner",
-				["ac"] = "@class.outer",
-				["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
-				["as"] = { query = "@local.scope", query_group = "locals", desc = "Select language scope" },
-			  },
-			  selection_modes = {
-				['@parameter.outer'] = 'v', -- charwise
-				['@function.outer'] = 'V', -- linewise
-				['@class.outer'] = '<c-v>', -- blockwise
-			  },
-			  include_surrounding_whitespace = true,
-			},
+    'nvim-treesitter/nvim-treesitter-textobjects',
+    lazy = false,
+    dependencies = {"nvim-treesitter/nvim-treesitter"},
+    priority = 100,
+    config = function()
+        require'nvim-treesitter.configs'.setup {
+            textobjects = {
+                select = {
+                    enable = true,
+                    lookahead = true,
+                    keymaps = {
+                        ['aa'] = '@parameter.outer',
+                        ['ia'] = '@parameter.inner',
 
-			move = {
-				  enable = true,
-				  set_jumps = true, -- whether to set jumps in the jumplist
-				  goto_next_start = {
-					["]m"] = "@function.outer",
-					["]]"] = { query = "@class.outer", desc = "Next class start" },
-					["]o"] = "@loop.*",
-					["]s"] = { query = "@local.scope", query_group = "locals", desc = "Next scope" },
-					["]z"] = { query = "@fold", query_group = "folds", desc = "Next fold" },
-				  },
-				  goto_next_end = {
-					["]M"] = "@function.outer",
-					["]["] = "@class.outer",
-				  },
-				  goto_previous_start = {
-					["[m"] = "@function.outer",
-					["[["] = "@class.outer",
-				  },
-				  goto_previous_end = {
-					["[M"] = "@function.outer",
-					["[]"] = "@class.outer",
-				  },
-				  goto_next = {
-					["]d"] = "@conditional.outer",
-				  },
-				  goto_previous = {
-					["[d"] = "@conditional.outer",
-				  }
-				},
+                        ['ic'] = '@conditional.inner',
+                        ['ac'] = '@conditional.outer',
 
-			  },
+                        ['as'] = '@block.outer',
+                        ['is'] = '@block.inner',
+                        --["as"] = { query = "@local.scope.inner", query_group = "locals", desc = "Select language scope" },
+
+                        ["af"] = "@function.outer",
+                        ["if"] = "@function.inner",
+
+                        ["aC"] = "@class.outer",
+                        ["iC"] = { query = "@class.inner", desc = "Select inner part of a class region" },
+
+                        --["iz"] = { query = "@fold", query_group = "folds", desc = "Next fold" },
+                    },
+                    selection_modes = {
+                        ['@parameter.outer'] = 'v', -- charwise
+                        ['@function.outer'] = 'V', -- linewise
+                        ['@class.outer'] = '<c-v>', -- blockwise
+                    },
+                    include_surrounding_whitespace = true,
+                },
+
+                move = {
+                    enable = true,
+                    set_jumps = true, -- whether to set jumps in the jumplist
+                    goto_next_start = {
+                        -- ["]m"] = "@function.outer",
+                        -- ["]]"] = { query = "@class.outer", desc = "Next class start" },
+                        -- ["]o"] = "@loop.*",
+                        -- ["]z"] = { query = "@fold", query_group = "folds", desc = "Next fold" },
+
+                        ["[s"] = { query = "@block.outer"},
+                        -- ["[s"] = { query = "@local.scope", query_group = "locals", desc = "Next scope" },
+                    },
+                    goto_next_end = {
+                        -- ["]S"] = { query = "@local.scope", query_group = "locals", desc = "Previous scope" },
+                        -- ["]f"] = "@function.outer",
+                        -- ["]["] = "@class.outer",
+                    },
+                    goto_previous_start = {
+                        ["]s"] = { query = "@block.outer"},
+                        -- ["]s"] = { query = "@local.scope", query_group = "locals", desc = "Previous scope" },
+
+                        -- ["[f"] = "@function.outer",
+                        -- ["[["] = "@class.outer",
+                    },
+                    goto_previous_end = {
+                        -- ["]S"] = { query = "@local.scope", query_group = "locals", desc = "Previous scope" },
+                        -- ["[M"] = "@function.outer",
+                        -- ["[]"] = "@class.outer",
+                    },
+                    goto_next = {
+                        ["[f"] = "@function.outer",
+                        ["[c"] = "@conditional",
+                    },
+                    goto_previous = {
+                        ["]f"] = "@function.outer",
+                        ["]c"] = "@conditional",
+                    }
+                },
+
+            },
 
 
 
-		}
-	end
+        }
+    end
 }
