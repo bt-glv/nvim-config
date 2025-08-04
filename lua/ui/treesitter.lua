dbed46a4da50538c748333f041eecfb01b61a4f2
glv <brunoglv@proton.me>
2025-08-04 19:26:33 -0300

    fix(treesitter): make diff and go parsers work
    - separate config and opts
    - add diff and markdown as required parsers
    - remove treesitter as a fold source

--- lua/ui/treesitter.lua
+++ lua/ui/treesitter.lua
@@ -5,21 +5,36 @@ return {
     lazy = false,
     dependencies = {},
     priority = 100,
+	opts = {
+		ensure_installed = {
+			'javascript',
+			'html',
+			'css',
+			'python',
+			'go',
+			'c',
+			'lua',
+			'vim',
+			'vimdoc',
+			'query',
+			'bash',
+			'json',
+			'diff',
+			'markdown',
+		},
+		sync_install = false,
+		auto_install = true,
+		highlight = {
+			enable = true,
+			additional_vim_regex_highlighting = false,
+		},
+		ignore_install = {},
+	},
     config = function()
-        require'nvim-treesitter.configs'.setup {
-            ensure_installed = {"javascript","html","css","python", "go", "c", "lua", "vim", "vimdoc", "query","bash","json" },
-
-            sync_install = false,
-            auto_install = true,
-            highlight = {
-                enable = true,
-                additional_vim_regex_highlighting = false,
-            },
-        }
-
-        vim.o.foldmethod = 'expr'
-        vim.o.foldexpr = 'nvim_treesitter#foldexpr()'
-        vim.o.foldlevelstart = 99
-
-    end -- END Config function 
+		-- this might be causing some weird treesitter parser errors
+		-- involving parser initialization with 'diff' and 'golang'
+		--vim.o.foldmethod = 'expr'
+		--vim.o.foldexpr = 'nvim_treesitter#foldexpr()'
+		--vim.o.foldlevelstart = 99
+	end,
 }
