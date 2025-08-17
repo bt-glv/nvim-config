a4c7b1fb9ed477494751a463e3ac1f63a392fbbf
bt-glv <bruno@glv.sh>
2025-08-17 17:56:15 -0300

    feat(DAP): add debugger support
    - Add and test go debug adapter

--- lua/debug/nvim-dap.lua
+++ lua/debug/nvim-dap.lua
@@ -1,8 +1,34 @@
 
 return {
 	'mfussenegger/nvim-dap',
-	lazy = false,
-	-- dependencies = {},
-	-- opts = {},
-	-- config = function() end
+	event = 'VeryLazy', -- temporary
+	dependencies = {
+		"nvim-neotest/nvim-nio",
+
+		-- debugger UI
+		'rcarriga/nvim-dap-ui',
+
+		-- Renders variable values on the buffer
+		'theHamsta/nvim-dap-virtual-text',
+
+		-- Fully handles "delve" debbuger required config
+		'leoluz/nvim-dap-go',
+	},
+	opts = {},
+
+	-- docs to set up debuggers
+	-- https://codeberg.org/mfussenegger/nvim-dap/wiki/Debug-Adapter-installation
+	config = function()
+		require("dap-go").setup()
+		local dapui = require('dapui')
+		dapui.setup()
+
+		-- TODO: add remaps for: stopping
+		-- TODO: dapuitoggle 
+		km('n', '<leader>dt', function() dapui.toggle() end , 	{noremap=true})
+		km('n', '<leader>dC', function() dapui.close() end , 	{noremap=true})
+		km('n', '<leader>db', ':DapToggleBreakpoint<CR>', 		{noremap=true})
+		km('n', '<leader>dc', ':DapContinue<CR>', 				{noremap=true})
+		km('n', '<leader>dr', function() dapui.open({reset = true}) end, {noremap=true})
+	end
 }
