2001fc0e349d46f50165251dd8e63683e3e88316
bt-glv <bruno@glv.sh>
2025-10-03 18:56:23 -0300

    WIP(cmp): completion mapping experiments

--- lua/lsp/nvim_cmp.lua
+++ lua/lsp/nvim_cmp.lua
@@ -1,4 +1,5 @@
 
+
 return {
 	'hrsh7th/nvim-cmp',
 	lazy = false,
@@ -25,30 +26,41 @@ return {
 				documentation = cmp.config.window.bordered(),
 			},
 			mapping = cmp.mapping.preset.insert({
+				-- ['<C-a>'] = cmp.mapping.complete(),
 				['<C-j>'] = cmp.mapping.scroll_docs(-4),
 				['<C-k>'] = cmp.mapping.scroll_docs(4),
-				['<C-e>'] = cmp.mapping.abort(),
 
-				-- I'm completely unsure about what this does
-				['<C-Space>'] = cmp.mapping.complete(),
 
 				-- default autocomplete selection commands
 				['<C-p>'] = cmp.mapping.select_prev_item(),
 				['<C-n>'] = cmp.mapping.select_next_item(),
+				['<C-e>'] = cmp.mapping.abort(),
 
+				-- ['<C-Space>'] = cmp.mapping.abort(),
 				['<CR>'] = cmp.mapping(function(fallback)
 					if cmp.visible() then
 						if luasnip.expandable() then
 							luasnip.expand()
 						else
-							cmp.confirm({
-								select = true,
-							})
+							cmp.confirm({ select = true, })
 						end
 					else
 						fallback()
 					end
 				end),
+				--
+
+				-- ['<C-Space>'] = cmp.mapping(function(fallback) -- experimental
+				-- 	if cmp.visible() then
+				-- 		if luasnip.expandable() then
+				-- 			luasnip.expand()
+				-- 		else
+				-- 			cmp.confirm({ select = true, })
+				-- 		end
+				-- 	else
+				-- 		fallback()
+				-- 	end
+				-- end),
 
 				['<Tab>'] = cmp.mapping(function(fallback)
 					if cmp.visible() then
@@ -75,23 +87,23 @@ return {
 				end, { "i", "s" }),
 
 				-- experimental
-				["<A-k>"] = cmp.mapping(
-					function(fallback)
-						local luasnip = require('luasnip')
-						if luasnip.expand_or_jumpable() then
-							luasnip.expand_or_jump()
-						end
-					end, { 'i', 's' }
-				),
-				["<A-j>"] = cmp.mapping(
-					function()
-						local luasnip = require('luasnip')
-						if luasnip.jumpable(-1) then
-							luasnip.jump(-1)
-						end
-					end, { 'i', 's' }
-				)
-
+				-- ["<A-k>"] = cmp.mapping(
+				-- 	function(fallback)
+				-- 		local luasnip = require('luasnip')
+				-- 		if luasnip.expand_or_jumpable() then
+				-- 			luasnip.expand_or_jump()
+				-- 		end
+				-- 	end, { 'i', 's' }
+				-- ),
+				-- ["<A-j>"] = cmp.mapping(
+				-- 	function()
+				-- 		local luasnip = require('luasnip')
+				-- 		if luasnip.jumpable(-1) then
+				-- 			luasnip.jump(-1)
+				-- 		end
+				-- 	end, { 'i', 's' }
+				-- )
+				--
 			}),
 			sources = cmp.config.sources({
 				{ name = 'nvim_lsp' },
@@ -101,6 +113,33 @@ return {
 				{ name = 'buffer' },
 			})
 		})
+
+		-- [IDEAS]
+		-- * I could try making this a cmp mapping
+		-- * Create a unstable branch for this config to avoided
+		-- having to deal with this kind of problem at unoportune times
+		--
+		-- [unrelated]
+		-- make a clipboard snippet
+
+		vim.keymap.set('i', '<A- >', function()
+			local cmp = require('cmp')
+			if cmp.visible() then
+				cmp.abort()
+				return
+			end
+
+			vim.api.nvim_feedkeys("", "t", false)
+		end, {noremap = true})
+
+
+		-- Autopairs related
+		-- local autopairs = require('nvim-autopairs.completion.cmp')
+		-- 	autopairs.on_confirm_done(),
+		-- cmp.event:on(
+		-- 	"confirm_done",
+		-- )
+
 
 
 	end,
