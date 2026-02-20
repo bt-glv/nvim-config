14089e61a976390575730ea04bd131bac49987d1
bt-glv <bruno@glv.sh>
2026-02-19 23:32:22 -0300

    fix(treesitter): markdown code block styling

--- lua/ui/treesitter.lua
+++ lua/ui/treesitter.lua
@@ -1,40 +1,45 @@
 
 return {
     'nvim-treesitter/nvim-treesitter',
-    run = ":TSUpdate",
-    lazy = false,
+    run          = ":TSUpdate",
+    lazy         = false,
     dependencies = {},
-    priority = 100,
+    priority     = 100,
+
+	main = "nvim-treesitter.configs",
 	opts = {
 		ensure_installed = {
-			'javascript',
-			'html',
-			'css',
-			'python',
-			'go',
-			'c',
 			'lua',
 			'vim',
 			'vimdoc',
-			'query',
-			'bash',
-			'json',
-			'diff',
+
+			'javascript',
+			'html',
+			'css',
+			'typescript',
+
+			'markdown_inline',
 			'markdown',
+			'latex',
+
+			'java',
+			'python',
+			'go',
+			'c',
+			'bash',
+			'diff',
+			'query',
+
+			'json',
+			'xml',
+			'yaml'
 		},
-		sync_install = false,
-		auto_install = true,
 		highlight = {
-			enable = true,
+			enable                            = true,
 			additional_vim_regex_highlighting = false,
 		},
+		sync_install   = false,
+		auto_install   = true,
 		ignore_install = {},
 	},
-    config = function()
-		-- this might be causing some weird treesitter parser errors
-		-- involving parser initialization with 'diff' and 'golang'
-		vim.o.foldmethod = 'expr'
-		vim.o.foldexpr = 'nvim_treesitter#foldexpr()'
-		vim.o.foldlevelstart = 99
-	end,
 }
