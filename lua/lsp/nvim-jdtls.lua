85342c65da52b3c8889c78e04bd9ec28d774d1b8
bt-glv <bruno@glv.sh>
2026-05-07 16:10:40 -0300

    feat: Add nvim-jdtls plugin

---
+++ lua/lsp/nvim-jdtls.lua
@@ -0,0 +1,13 @@
+return {
+	"mfussenegger/nvim-jdtls",
+	dependencies = {'hrsh7th/nvim-cmp'},
+	ft           = { 'java' },
+
+	-- opts         = {},
+	-- config = function() end,
+	-- init = function() end,
+	-- lazy = false,
+	-- keys = {},
+	-- event = "",
+	-- cmd = "",
+}
