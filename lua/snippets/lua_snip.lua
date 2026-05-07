4bcead59b6b0af8a603b5b170567cf15f05d0558
bt-glv <bruno@glv.sh>
2026-05-07 16:12:23 -0300

    feat(lua_snip): Enable snippet history - Also, add support for more snippets

--- lua/snippets/lua_snip.lua
+++ lua/snippets/lua_snip.lua
@@ -1,25 +1,31 @@
 
 local function add_user_snippets()
 	local ls = require('luasnip')
-	ls.add_snippets("lua",  require('snippets.lang.lua'))
-	ls.add_snippets("java",  require('snippets.lang.java'))
+	ls.add_snippets("lua",         require('snippets.lang.lua'     ))
+	ls.add_snippets("java",        require('snippets.lang.java'    ))
+	ls.add_snippets("markdown",    require('snippets.lang.markdown'))
+	ls.add_snippets("c",           require('snippets.lang.c'       ))
+	ls.add_snippets("vim",         require('snippets.lang.vim'     ))
+	ls.add_snippets("cpp",         require('snippets.lang.cpp'     ))
 end
 
 return {
 	"L3MON4D3/LuaSnip",
-	version = "v2.*",
-	build = "make install_jsregexp", -- install jsregexp (optional!).
+	version      = "v2.*",
+	build        = "make install_jsregexp", -- install jsregexp (optional!).
 	dependencies = { "rafamadriz/friendly-snippets", },	-- pre-made snippets
-	config = function()
+	config       = function()
 		local types = require("luasnip.util.types")
 		require('luasnip').setup(
 			{
-				keep_roots = true,
-				link_roots = true,
+				history             = true,
+				update_events       = "TextChanged,TextChangedI",
+				delete_check_events = "TextChanged",
+
+				keep_roots    = true,
+				link_roots    = true,
 				link_children = true,
 
-				update_events = "TextChanged,TextChangedI",
-				delete_check_events = "TextChanged",
 				ext_opts = {
 					[types.choiceNode] = {
 						active = {
@@ -27,9 +33,9 @@ return {
 						},
 					},
 				},
-				ext_base_prio = 300,
-				ext_prio_increase = 1,
-				enable_autosnippets = true,
+				ext_base_prio        = 300,
+				ext_prio_increase    = 1,
+				enable_autosnippets  = true,
 				store_selection_keys = "<Tab>",
 				ft_func = function()
 					return vim.split(vim.bo.filetype, ".", true)
